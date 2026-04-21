1851242a21135da55f2d70e071a50e8d69ab66e2
drodrigor <danierrodrigo@gmail.com>
2026-04-21 12:52:08 +0200

    Initial commit

---
+++ PuntuaShow/src/main/java/com/DRR/PuntuaShow/PuntuaShowApplication.java
@@ -0,0 +1,13 @@
+package com.DRR.PuntuaShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntuaShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntuaShowApplication.class, args);
+	}
+
+}
---
+++ PuntuaShow/src/test/java/com/DRR/PuntuaShow/PuntuaShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DRR.PuntuaShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntuaShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
