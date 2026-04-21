58c692583beea1624fe0886961fe566650e5c269
drodrigor <danierrodrigo@gmail.com>
2026-04-21 18:55:15 +0200

    Se añaden las anotaciónes @Entity y @Table

--- PuntuaShow/src/main/java/com/DRR/PuntuaShow/entity/Serie.java
+++ PuntuaShow/src/main/java/com/DRR/PuntuaShow/entity/Serie.java
@@ -1,4 +1,16 @@
 package com.DRR.PuntuaShow.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * La anotación @Entity indica que esta clase
+ * será mapeada a una tabla en la base de datos.
+
+ * La anotación @Table permite definir el nombre de la tabla.
+ * Si no lo pones, usaría "serie" por defecto.
+ */
+@Entity
+@Table(name = "series")
 public class Serie {
 }
