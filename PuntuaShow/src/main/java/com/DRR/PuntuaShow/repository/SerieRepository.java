fe516a22829982a26e452dfe888d374cb2c14f11
drodrigor <danierrodrigo@gmail.com>
2026-04-30 18:22:22 +0200

    Método findByNombre(String nombre)

--- PuntuaShow/src/main/java/com/DRR/PuntuaShow/repository/SerieRepository.java
+++ PuntuaShow/src/main/java/com/DRR/PuntuaShow/repository/SerieRepository.java
@@ -3,6 +3,8 @@ package com.DRR.PuntuaShow.repository;
 import com.DRR.PuntuaShow.entity.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Un Repository en Spring Data es una interfaz (NO una clase).
  *
@@ -10,4 +12,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * en tiempo de ejecución.
  */
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+    // =========================
+    // MÉTODOS PERSONALIZADOS
+    // =========================
+
+    /**
+     * Spring genera automáticamente la query a partir del nombre.
+     *
+     * findBy + NombreCampo
+     *
+     * Esto generará algo equivalente a:
+     * SELECT * FROM series WHERE nombre = ?
+     */
+    List<Serie> findByNombre(String nombre);
+
 }
