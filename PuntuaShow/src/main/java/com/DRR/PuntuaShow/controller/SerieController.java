a8749c0f2c0692aa5484266f966651271304556c
drodrigor <danierrodrigo@gmail.com>
2026-05-06 15:35:20 +0200

    Se añade las anotaciones RestController y RequestMapping

--- PuntuaShow/src/main/java/com/DRR/PuntuaShow/controller/SerieController.java
+++ PuntuaShow/src/main/java/com/DRR/PuntuaShow/controller/SerieController.java
@@ -1,4 +1,23 @@
 package com.DRR.PuntuaShow.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RestController indica que esta clase es un controlador REST.
+ *
+ * Combina:
+ * - @Controller
+ * - @ResponseBody (devuelve JSON automáticamente)
+ */
+@RestController
+
+/**
+ * Define la ruta base de todos los endpoints
+ *
+ * Ejemplo:
+ * http://localhost:1010/api/series
+ */
+@RequestMapping("/api/series")
 public class SerieController {
 }
