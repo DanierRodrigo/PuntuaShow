48b796382d5d87a1ded7b259f5edcc5e0e3cb8e0
drodrigor <danierrodrigo@gmail.com>
2026-05-13 16:06:59 +0200

    getSerieById()

--- PuntuaShow/src/main/java/com/DRR/PuntuaShow/controller/SerieController.java
+++ PuntuaShow/src/main/java/com/DRR/PuntuaShow/controller/SerieController.java
@@ -4,10 +4,12 @@ import com.DRR.PuntuaShow.entity.Serie;
 import com.DRR.PuntuaShow.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @RestController indica que esta clase es un controlador REST.
@@ -43,6 +45,18 @@ public class SerieController {
     @GetMapping
     public List<Serie> getAllSeries() {
         return serieService.findAll();
+    }
+
+    // =========================
+    // GET - Buscar por ID
+    // =========================
+
+    /**
+     * GET /api/series/{id}
+     */
+    @GetMapping("/{id}")
+    public Optional<Serie> getSerieById(@PathVariable Long id) {
+        return serieService.findById(id);
     }
 
 }
